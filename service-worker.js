65f68a4fa44535d8a8775a6009f40ad091c4e123
Prashant Andani <pandani@innominds.com>
2017-12-22 18:43:42 +0530

    Adding initial files

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  console.log("Will the service worker register?");
+  navigator.serviceWorker.register('service-worker.js')
+    .then(function(reg){
+      console.log("Yes, it did.");
+    }).catch(function(err) {
+      console.log("No it didn't. This happened: ", err)
+    });
+  }
